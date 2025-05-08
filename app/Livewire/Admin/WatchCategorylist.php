b5cfbfe407436c637149ef5498c58b90e0a30f96
amhar4u <muhammedamhar@gmail.com>
2025-05-08 19:54:46 +0530

    Design and implement Watch Categorylist

---
+++ app/Livewire/Admin/WatchCategorylist.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Livewire\Admin;
+use Exception;
+use Livewire\Component;
+use Livewire\Attributes\On;
+use App\Models\CategoryList;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+
+#[Title("Watch Category List")]
+#[Layout('components.layouts.admin')]
+class WatchCategorylist extends Component
+{
+    public $categoryName;
+    public function render()
+    {
+        $categories = CategoryList::orderBy('id','desc')->get();
+        return view('livewire.admin.watch-categorylist',[
+            'categories'=> $categories,
+        ]);
+    }
+
+    public function createCategory(){
+        $this->reset();
+        $this->js("$('#createCategoryModal').modal('show')");
+    }
+
+    public function saveCategory(){
+        $this->validate([
+            'categoryName' => 'required|unique:category_lists,category_name',
+        ]);
+        try{
+            
+            CategoryList::create([
+                'category_name' => $this->categoryName,
+            ]);
+            $this->js("Swal.fire('Success!', 'Category Created Successfully', 'success')");
+        }catch(Exception $e){
+            $this->js("Swal.fire('Error!', '".$e->getMessage()."', 'error')");
+        }
+        
+        $this->js('$("#createCategoryModal").modal("hide")');
+        
+    }
+
+    public $editCategoryId;
+    public $editCategoryName;
+  
+    public function editCategory($id){
+        $category = CategoryList::find($id);
+        $this->editCategoryName = $category->category_name;
+        $this->editCategoryId = $category->id;
+        
+        $this->js("$('#editCategoryModal').modal('show')");
+    }
+
+    public function updateCategory($id){
+        try{
+            $this->validate([
+                'editCategoryName' => 'required|unique:category_lists,category_name',
+            ]);
+            CategoryList::where('id', $id)->update([
+                'category_name' => $this->editCategoryName,
+            ]);
+            $this->js('$("#editCategoryModal").modal("hide")');
+            $this->js("Swal.fire('Success!', 'Category Updated Successfully', 'success')");
+        }catch(Exception $e){
+            // log($e->getMessage());
+            $this->js("Swal.fire('Error!', '".$e->getMessage()."', 'error')");
+        }
+        
+        $this->js('$("#editCategoryModal").modal("hide")');
+    }
+    
+    public $deleteId;
+    public function confirmDelete($id)
+    {
+        $this->deleteId = $id;
+        $this->dispatch('confirm-delete');
+    }
+    #[On('confirmDelete')]
+    public function deleteCategory(){
+        try{
+            CategoryList::where('id', $this->deleteId )->delete();
+        }catch(Exception $e){
+            // log($e->getMessage());
+            $this->js("Swal.fire('Error!', '".$e->getMessage()."', 'error')");
+        }
+    }
+}
--- resources/views/livewire/admin/watch-categorylist.blade.php
+++ resources/views/livewire/admin/watch-categorylist.blade.php
@@ -2,10 +2,10 @@
     <div class="container-fluid">
         <div class="card shadow-sm">
             <div class="card-header d-flex justify-content-between align-items-center flex-wrap bg-light">
-                <h4 class="card-title mb-2 mb-md-0">Watch Brand List</h4>
+                <h4 class="card-title mb-2 mb-md-0">Watch Category List</h4>
                 <div class="card-tools">
-                    <button class="btn btn-primary" wire:click="createBrand">
-                        <i class="bi bi-plus-circle me-1"></i> Create Watch Brand
+                    <button class="btn btn-primary" wire:click="createCategory">
+                        <i class="bi bi-plus-circle me-1"></i> Create Watch Category
                     </button>
                 </div>
             </div>
@@ -14,22 +14,22 @@
                     <thead>
                         <tr>
                             <th class="text-center">#</th>
-                            <th class="text-center">Brand Name</th>
+                            <th class="text-center">Category Name</th>
                             <th class="text-center">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @if ($brands->count() > 0)
-                            @foreach ($brands as $brand)
+                        @if ($categories->count() > 0)
+                            @foreach ($categories as $category)
                                 <tr>
                                     <td class="text-center">{{ $loop->iteration }}</td>
-                                    <td class="text-center">{{ $brand->brand_name }}</td>
+                                    <td class="text-center">{{ $category->category_name }}</td>
                                     <td class="text-center">
                                         <button class="btn btn-sm btn-primary me-2"
-                                            wire:click="editBrand({{ $brand->id }})">
+                                            wire:click="editCategory({{ $category->id }})">
                                             <i class="bi bi-pencil-square"></i> Edit
                                             <button class="btn btn-sm btn-danger"
-                                                wire:click="confirmDelete({{ $brand->id }})">
+                                                wire:click="confirmDelete({{ $category->id }})">
                                                 <i class="bi bi-trash"></i> Delete
                                             </button>
                                     </td>
@@ -38,7 +38,7 @@
                         @else
                             <td colspan="4" class="text-center">
                                 <div class="alert alert-primary bg-opacity-10 my-2">
-                                    <i class="bi bi-info-circle me-2"></i> No watches Brands found.
+                                    <i class="bi bi-info-circle me-2"></i> No watches Categories found.
                                 </div>
                             </td>
                         @endif
@@ -46,22 +46,22 @@
                 </table>
             </div>
         </div>
-        {{-- Create Brand Model --}}
-        <div wire:ignore.self class="modal fade" id="createBrandModal" tabindex="-1" aria-labelledby="createBrandModalLabel"
+        {{-- Create Category Model --}}
+        <div wire:ignore.self class="modal fade" id="createCategoryModal" tabindex="-1" aria-labelledby="createCategoryModalLabel"
             aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header bg-primary text-white">
-                        <h1 class="modal-title fs-5" id="createBrandModalLabel">Add Brand</h1>
+                        <h1 class="modal-title fs-5" id="createCategoryModalLabel">Add Category</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body p-3">
                         <div class="row">
 
                             <div class="mb-3">
-                                <label for="brandName" class="form-label">Brand Name</label>
-                                <input type="text" class="form-control" id="brandName" wire:model="brandName">
-                                @error('brandName')
+                                <label for="categoryName" class="form-label">Category Name</label>
+                                <input type="text" class="form-control" id="categoryName" wire:model="categoryName">
+                                @error('categoryName')
                                     <span class="text-danger">*{{ $message }}</span>
                                 @enderror
                             </div>
@@ -69,29 +69,29 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary" wire:click="saveBrand">Add Brand</button>
+                        <button type="button" class="btn btn-primary" wire:click="saveCategory">Add Brand</button>
                     </div>
                 </div>
             </div>
         </div>
-        {{-- End Create Brand Model --}}
+        {{-- End Create Category Model --}}
     </div>
-    {{-- Edit Brand Model --}}
-    <div wire:ignore.self  class="modal fade" id="editBrandModal" tabindex="-1" aria-labelledby="editBrandModalLabel"
+    {{-- Edit Category Model --}}
+    <div wire:ignore.self  class="modal fade" id="editCategoryModal" tabindex="-1" aria-labelledby="editCategoryModalLabel"
         aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header bg-primary text-white">
-                    <h1 class="modal-title fs-5" id="editBrandModalLabel">Add Brand</h1>
+                    <h1 class="modal-title fs-5" id="editCategoryModalLabel">Add Category</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body p-3">
                     <div class="row">
 
                         <div class="mb-3">
-                            <label for="editBrandName" class="form-label">Brand Name</label>
-                            <input type="text" class="form-control" id="editBrandName" wire:model="editBrandName">
-                            @error('editBrandName')
+                            <label for="editCategoryName" class="form-label">Category Name</label>
+                            <input type="text" class="form-control" id="editCategoryName" wire:model="editCategoryName">
+                            @error('editCategoryName')
                                 <span class="text-danger">*{{ $message }}</span>
                             @enderror
                         </div>
@@ -99,7 +99,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" wire:click="updateBrand({{$editBrandId}})">Update Brand</button>
+                    <button type="button" class="btn btn-primary" wire:click="updateCategory({{$editCategoryId}})">Update Category</button>
                 </div>
             </div>
         </div>
