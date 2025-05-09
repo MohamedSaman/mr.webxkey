ae91357d15fdbf397cff8dbadc1604ad6d8124a0
amhar4u <muhammedamhar@gmail.com>
2025-05-09 11:13:20 +0530

    Modify the relationships

--- app/Models/WatchDetail.php
+++ app/Models/WatchDetail.php
@@ -10,32 +10,37 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 class WatchDetail extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
-        'code', 'name', 'model', 'color', 'made_by', 'gender',
-        'type', 'movement', 'dial_color', 'strap_color', 'strap_material',
-        'case_diameter_mm', 'case_thickness_mm', 'glass_type', 
-        'water_resistance', 'features', 'image', 'warranty',
-        'description', 'barcode', 'status', 'location',
-        'brand_id', 'category_id', 'supplier_id'
+        'code',
+        'name',
+        'model',
+        'color',
+        'made_by',
+        'gender',
+        'type',
+        'movement',
+        'dial_color',
+        'strap_color',
+        'strap_material',
+        'case_diameter_mm',
+        'case_thickness_mm',
+        'glass_type',
+        'water_resistance',
+        'features',
+        'image',
+        'warranty',
+        'description',
+        'barcode',
+        'status',
+        'location',
+        'brand',
+        'category',
+        'discription',
+        'supplier_id'
     ];
-    
-    /**
-     * Get the brand of the watch
-     */
-    public function brand(): BelongsTo
-    {
-        return $this->belongsTo(WatchBrand::class);
-    }
-    
-    /**
-     * Get the category of the watch
-     */
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(WatchCategory::class);
-    }
-    
+
+ 
     /**
      * Get the supplier of the watch
      */
@@ -43,7 +48,7 @@ class WatchDetail extends Model
     {
         return $this->belongsTo(WatchSupplier::class);
     }
-    
+
     /**
      * Get the price information for the watch
      */
@@ -51,7 +56,7 @@ class WatchDetail extends Model
     {
         return $this->hasOne(WatchPrice::class, 'watch_id');
     }
-    
+
     /**
      * Get the stock information for the watch
      */
