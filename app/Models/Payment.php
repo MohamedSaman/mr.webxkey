7fb21926712c7f107187958fdbb3a2a31f666a15
amhar4u <muhammedamhar@gmail.com>
2025-05-13 22:58:22 +0530

    Payment model and migration

---
+++ app/Models/Payment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sale_id',
+        'amount',
+        'payment_method',
+        'payment_reference',
+        'card_number',
+        'bank_name',
+        'is_completed',
+        'payment_date',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'payment_date' => 'datetime',
+        'due_date' => 'date',
+        'is_completed' => 'boolean',
+    ];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+}
